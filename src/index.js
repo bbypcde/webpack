af97eaf7ebb2bd5ab6c3662ca86b03845ebc4c75
bbypcde <bijingying198911@163.com>
2018-02-03 22:54:44 +0800

    webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import css from './css/index.css';
+import sass from './sass/index.scss';
+import $ from 'jquery';
+document.write("jingyingde");
+let add=(x,y)=>x+y;
+$('img').css('height','100px');
